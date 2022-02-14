ad46e5cb5a6d95655c9d18ae08bfef6e25e67680
Shourav Rajbongshi <mrinmoyraj823@gmail.com>
2022-02-14 15:02:59 +0600

    CSS Framework added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
+import { Button } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+       <Button variant="contained">Contained</Button>
+
     </div>
   );
 }
